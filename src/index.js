c3814a5a3f37215708126cde212383a37c4b4c45
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-07 15:56:24 +0300

    Load scores from scores array in the javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import scores from './modules/scores.js';
+
+const dashboard = document.getElementById('scores-table');
+
+const loadScores = () => {
+  dashboard.innerHTML = scores.map(score => `<tr><td>${score.name}: ${score.score}</td></tr>`).join('');
+} 
+
+window.onload = () => {
+  loadScores();
+}
---
+++ src/modules/scores.js
@@ -0,0 +1,24 @@
+const scores = [
+  {
+    name: 'Wuletaw',
+    score: 200,
+  },
+  {
+    name: 'Safar',
+    score: 50,
+  },
+  {
+    name: 'Abebe',
+    score: 100,
+  },
+  {
+    name: 'Sentayehu',
+    score: 250,
+  },
+  {
+    name: 'Rio',
+    score: 100,
+  },
+];
+
+export default scores;
