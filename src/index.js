afbcf701c68070434a2fa3b64481a477dcfcf1dc
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-07 16:08:51 +0300

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import scores from './modules/scores.js';
 const dashboard = document.getElementById('scores-table');
 
 const loadScores = () => {
-  dashboard.innerHTML = scores.map(score => `<tr><td>${score.name}: ${score.score}</td></tr>`).join('');
-} 
+  dashboard.innerHTML = scores.map((score) => `<tr><td>${score.name}: ${score.score}</td></tr>`).join('');
+};
 
 window.onload = () => {
   loadScores();
-}
+};
